57a761df1315aaa176bf0c1595152cc9aa4ffaa6
Tebogo Nchabeleng <tebogo607@students.iie.ac.za>
2026-06-21 11:38:36 +0200

    added a reference list

--- Program.cs
+++ Program.cs
@@ -24,3 +24,28 @@ namespace CybersecurityChatbot
         }
     }
 }
+// 
+// REFERENCE LIST (Harvard style)
+
+// Farrell, J. (2018) Microsoft Visual C#: An Introduction to Object-Oriented
+//     Programming. 7th edn. Boston: Cengage Learning.
+
+// GeeksforGeeks (no date) C# Programming Language. Available at:
+//     https://www.geeksforgeeks.org/c-sharp-programming-language/
+//     (Accessed: 20 June 2026).
+
+// Microsoft (no date) C# documentation. Available at:
+//     https://learn.microsoft.com/en-us/dotnet/csharp/
+//     (Accessed: 20 June 2026).
+
+// Microsoft (no date) Windows Presentation Foundation (WPF) documentation.
+//     Available at: https://learn.microsoft.com/en-us/dotnet/desktop/wpf/
+//     (Accessed: 20 June 2026).
+
+// Oracle Corporation (no date) MySQL 8.0 Reference Manual. Available at:
+//     https://dev.mysql.com/doc/refman/8.0/en/
+//     (Accessed: 20 June 2026).
+
+// W3Schools (no date) C# Tutorial. Available at:
+//     https://www.w3schools.com/cs/
+//     (Accessed: 20 June 2026).
